f804dafb4bea39d2563743c64e182a4e639ae32f
Alexander Ostroverkhov <alexruw@gmail.com>
2015-09-10 15:51:27 +0300

    bug fix: spaces in path

--- LinuxJavaFixes.sh
+++ LinuxJavaFixes.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+path_to_jar=$HOME'/repos/LinuxJavaFixes/build/LinuxJavaFixes-1.0.0-SNAPSHOT.jar';
+ide_dir=$HOME'/Downloads/';
 
-for i in $(grep --exclude='*' --include='*64.vmoptions' -L -F 'LinuxJavaFixes' -r ~/Downloads/); do
-	echo "-javaagent:\"$HOME/repos/LinuxJavaFixes/build/LinuxJavaFixes-1.0.0-SNAPSHOT.jar\"" >> "$i";
-	echo "$i";
+grep --exclude='*' --include='*64.vmoptions' -L -F 'LinuxJavaFixes' -r "$ide_dir" | while read -r line ; do
+    echo "-javaagent:\"$path_to_jar\"" >> "$line";
+    echo "$line";
 done
