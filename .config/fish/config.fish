7350b65568f4dcc9581a601fd672e68982ba10a7
Alexander Ostroverkhov <alexruw@gmail.com>
2018-10-24 09:32:02 +0300

    fishshell equivalent to .bashrc
    https://fishshell.com/docs/current/faq.html

---
+++ .config/fish/config.fish
@@ -0,0 +1,29 @@
+set PATH $PATH /usr/local/bin
+export PATH
+
+#Алиасы для раскраски вывода команд с помощью grc.
+alias ping='grc ping'
+alias ping6='grc ping6'
+alias traceroute='grc traceroute'
+alias make='grc make'
+alias diff='grc diff'
+alias cvs='grc cvs'
+alias netstat='grc netstat'
+alias logc='grc cat'
+alias logt='grc tail'
+alias logh='grc head'
+alias less='less --RAW-CONTROL-CHARS'
+
+#обновление списка пакетов и самих пакетов одной командой
+alias upd='sudo apt update; sudo apt upgrade; sudo apt clean; sudo apt autoclean'
+
+#git
+alias g="git"
+alias s="g status -s"
+alias gp="g pull"
+alias gl='g log --pretty=format:"%h - %cd : %s" --graph'
+
+#ф-я для поиска строки в проекте на Yii2
+function gr
+    grep -HnrF --exclude-dir={.idea,.git,vendor,runtime,rbac} $1 .
+end
